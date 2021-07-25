ca0890b1fe516bc8982fa9fa9359c375404fcb20
snmln <designbymalone@gmail.com>
2021-07-25 18:49:32 -0400

    adding login functionality and partial data layer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
 import Login from './Login.js'
+import React, {useEffect, useState} from "react";
+import {getTokenFromUrl} from './spotify';
+import SpotifyWebApi from "spotify-web-api-js"; 
+import Player from './Player'
+import {useDataLayerValue} from './DataLayer'
+
+const spotify= new SpotifyWebApi();
+
 function App() {
+
+  const [token, setToken] = useState(null);
+const [{}, dispatch] = useDataLayerValue();
+
+useEffect(()=>{
+   const hash = getTokenFromUrl();
+   window.location.hash="";
+   const _token = hash.access_token;
+   if(_token){
+     setToken(_token)
+     spotify.setAccessToken(_token)
+     spotify.getMe().then(user=>{
+       console.log(user);
+       dispatch({
+         type: 'SET_USER',
+         user: user,
+         
+       })
+
+     })
+   }
+   console.log('I HAVE A TOKEN>>>>>>', _token);
+},[]);
+
+
+
+
   return (
     <div className="App">
-     <Login />
+      {
+        token ? (
+        <Player />) :
+        (     <Login />
+          )
+      }
     </div>
   );
 }
